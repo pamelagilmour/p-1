2d06f8a3d4220040b5e44781a72da1e520014361
Pamela Gilmour <homefolder@Pamelas-MacBook-Air.local>
2026-02-17 14:54:04 -0800

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -62,7 +62,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
-        "https://p-11-one.vercel.app/",  # Add your actual Vercel URL
+        "https://p-11-one.vercel.app",
         "https://*.vercel.app",  # Allow all Vercel preview deployments
     ],
     allow_credentials=True,
